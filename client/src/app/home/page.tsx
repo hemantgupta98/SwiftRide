0c9bf24b23b61cbd48b5b36c00034a145c971c31
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-26 19:14:47 +0530

    work on footer

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -3,7 +3,7 @@ import Hero from "../../components/layout/hero";
 import AppPreview from "../../components/ui/screen";
 import Features from "../../components/layout/feature";
 import CTA from "../../components/layout/cta";
-import Footer from "../../components/layout/feature";
+import Footer from "../../components/layout/footer";
 
 export default function Home() {
   return (
