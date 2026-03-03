dfea52593c97773c09bedbd31c4e91e4dc61e8a2
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-03-03 08:13:26 +0530

    work on rider promo page

--- client/src/app/CuRider/page.tsx
+++ client/src/app/CuRider/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Input } from "../../components/ui/input";
+import { Search } from "lucide-react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>ride</div>;
+  return (
+    <>
+      <div className=" min-h-screen rounded-2xl shadow-2xl m-5 p-5">
+        <div className="flex items-center">
+          <Search size={18} className="absolute ml-3" />
+          <Input
+            placeholder="Enter your pickup location"
+            className=" rounded-md pl-10 "
+          />
+        </div>
+        <div>
+          <Image src="/riderPromo.jpeg" alt="promo" height={200} width={200} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default page;
