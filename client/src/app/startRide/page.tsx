82815d6b270a7250a91cd14c2174bc560620709a
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-03-08 16:49:50 +0530

    delete descripiton text

--- client/src/app/startRide/page.tsx
+++ client/src/app/startRide/page.tsx
@@ -2,10 +2,8 @@
 import dynamic from "next/dynamic";
 import React, { useState } from "react";
 import { Input } from "../../components/ui/inputApi";
-import { LoadScript, Autocomplete } from "@react-google-maps/api";
-import { MapPin, Flag, MapPinned } from "lucide-react";
 
-const libraries: "places"[] = ["places"];
+import { MapPin, Flag, MapPinned } from "lucide-react";
 
 const LeafletMap = dynamic(() => import("../../components/ui/map"), {
   ssr: false,
@@ -109,62 +107,6 @@ const Page = () => {
         <div className="mt-6">
           <LeafletMap />
         </div>
-        {/*  <div className="rounded-2xl border border-orange-100 bg-linear-to-br from-orange-50 via-white mt-5 to-amber-50 p-5 shadow-2xl">
-            <div className="mb-4 flex flex-wrap gap-2">
-              <span className="rounded-full bg-orange-500 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-gray-600">
-                SwiftRide
-              </span>
-              <span className="rounded-full bg-green-100 px-3 py-1 text-xs font-semibold text-green-700">
-                Rider
-              </span>
-              <span className="rounded-full bg-blue-100 px-3 py-1 text-xs font-semibold text-blue-700">
-                Driver
-              </span>
-              <span className="rounded-full bg-rose-100 px-3 py-1 text-xs font-semibold text-rose-700">
-                Admin
-              </span>
-              <span className="rounded-full  px-3 py-1 text-xs font-semibold text-amber-700">
-                Affordable
-              </span>
-            </div>
-
-            <p className="text-[20px] leading-9 font-medium text-gray-700 tracking-wide [word-spacing:0.22em]">
-              <span className="rounded-md px-2 py-1 font-semibold text-orange-600 text-[22px]">
-                SwiftRide
-              </span>{" "}
-              is a ride-booking platform built to deliver fast, reliable, and
-              affordable transportation by connecting riders and drivers in one
-              seamless system. The platform works through three key roles:
-              <span className="rounded-md bg-green-100 px-2 py-1 font-semibold text-green-700">
-                Rider
-              </span>{" "}
-              (customer),
-              <span className="rounded-md bg-white px-2 py-1 font-semibold text-blue-700">
-                Driver
-              </span>{" "}
-              (partner), and
-              <span className="rounded-md bg-rose-100 px-2 py-1 font-semibold text-rose-700">
-                Admin
-              </span>
-              . Riders can register, log in, set pickup and drop locations, view
-              estimated fares, request rides instantly, track drivers in real
-              time, communicate when needed, and complete secure payments.
-              Riders are expected to share accurate location details and
-              maintain respectful behavior during each trip. Drivers accept ride
-              requests, provide safe and timely transportation, maintain valid
-              profile and vehicle details, follow traffic rules, reach pickup
-              points on time, and manage ride status, availability, and earnings
-              through the driver interface. Admin oversees the platform by
-              managing accounts, monitoring activities, resolving disputes,
-              ensuring security, verifying drivers, and maintaining service
-              quality. Overall,
-              <span className="rounded-md px-2 py-1 font-semibold text-orange-600">
-                SwiftRide
-              </span>{" "}
-              focuses on a secure, smooth, and convenient mobility experience
-              for both riders and drivers.
-            </p>
-          </div>* */}
       </div>
     </>
   );
