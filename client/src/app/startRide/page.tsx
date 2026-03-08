c9c53624678487d2ef20cd320d3afd4000124324
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-03-08 16:45:01 +0530

    map implementation complete

--- client/src/app/startRide/page.tsx
+++ client/src/app/startRide/page.tsx
@@ -1,41 +1,29 @@
 "use client";
-import React, { useRef } from "react";
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
 import { Input } from "../../components/ui/inputApi";
 import { LoadScript, Autocomplete } from "@react-google-maps/api";
 import { MapPin, Flag, MapPinned } from "lucide-react";
 
 const libraries: "places"[] = ["places"];
 
+const LeafletMap = dynamic(() => import("../../components/ui/map"), {
+  ssr: false,
+});
+
 const Page = () => {
-  const pickupRef = useRef<HTMLInputElement | null>(null);
-  const dropRef = useRef<HTMLInputElement | null>(null);
-  const pickupAutocompleteRef = useRef<google.maps.places.Autocomplete | null>(
-    null,
-  );
-  const dropAutocompleteRef = useRef<google.maps.places.Autocomplete | null>(
-    null,
-  );
+  const [pickup, setPickup] = useState("");
+  const [drop, setDrop] = useState("");
 
-  const onPickupLoad = (autocomplete: google.maps.places.Autocomplete) => {
-    pickupAutocompleteRef.current = autocomplete;
-  };
+  const searchLocation = async (query: string) => {
+    if (!query) return;
 
-  const onDropLoad = (autocomplete: google.maps.places.Autocomplete) => {
-    dropAutocompleteRef.current = autocomplete;
-  };
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/search?q=${query}&format=json`,
+    );
 
-  const onPickupPlaceChanged = () => {
-    const place = pickupAutocompleteRef.current?.getPlace();
-    const value =
-      place?.formatted_address || place?.name || pickupRef.current?.value || "";
-    console.log("Pickup:", value);
-  };
-
-  const onDropPlaceChanged = () => {
-    const place = dropAutocompleteRef.current?.getPlace();
-    const value =
-      place?.formatted_address || place?.name || dropRef.current?.value || "";
-    console.log("Drop:", value);
+    const data = await res.json();
+    console.log("Search Result:", data);
   };
 
   const getCurrentLocation = () => {
@@ -47,82 +35,81 @@ const Page = () => {
 
   return (
     <>
-      <LoadScript
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-        libraries={libraries}
-      >
-        <div className=" min-h-screen rounded-2xl shadow-2xl m-5 p-5 space-y-5">
-          <h1 className=" block text-orange-400 text-3xl font-semibold ">
-            Book your ride
-          </h1>
-          <form className=" mt-5 p-3">
-            <label className="block mb-2 font-semibold">Pickup location</label>
-            <div className="relative mb-4">
-              <MapPin
-                className="absolute left-3 top-1/2 -translate-y-1/2 text-green-600"
-                size={18}
-                color="#16a34a"
-              />
-              <Autocomplete
-                onLoad={onPickupLoad}
-                onPlaceChanged={onPickupPlaceChanged}
-              >
-                <Input
-                  ref={pickupRef}
-                  className="pl-10"
-                  placeholder="Pick-up location"
-                />
-              </Autocomplete>
-            </div>
+      <div className=" min-h-screen rounded-2xl shadow-2xl m-5 p-5 space-y-5">
+        <h1 className=" block text-orange-400 text-3xl font-semibold ">
+          Book your ride
+        </h1>
+        <form className="mt-5 p-3">
+          <label className="block mb-2 font-semibold">Pickup location</label>
+          <div className="relative mb-4">
+            <MapPin
+              className="absolute left-3 top-1/2 -translate-y-1/2 text-green-600"
+              size={18}
+            />
+            <Input
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value);
+                searchLocation(e.target.value);
+              }}
+              className="pl-10"
+              placeholder="Pick-up location"
+            />
+          </div>
 
-            <label className="block mb-2 font-semibold">Drop location</label>
-            <div className="relative">
-              <Flag
-                className="absolute text-red-600 left-3 top-1/2 -translate-y-1/2"
-                size={18}
-                color="#dc2626"
-              />
-              <Autocomplete
-                onLoad={onDropLoad}
-                onPlaceChanged={onDropPlaceChanged}
-              >
-                <Input
-                  ref={dropRef}
-                  className="pl-10"
-                  placeholder="Drop location"
-                />
-              </Autocomplete>
-            </div>
-            <div className="flex gap-5">
-              <button
-                type="button"
-                className=" mt-5 inline-flex items-center gap-2 bg-gray-200 rounded-md px-3 py-2 text-black font-semibold whitespace-nowrap"
-              >
-                Book Ride
-              </button>
-              <button
-                type="button"
-                className=" mt-5 inline-flex items-center gap-2 bg-gray-200 rounded-md px-3 py-2 text-red-400 font-semibold whitespace-nowrap"
-              >
-                Cancel Ride
-              </button>
-            </div>
-            <div className="flex items-center gap-4 my-6">
-              <div className="flex-1 h-px bg-gray-200" />
-              <div className="flex-1 h-px bg-gray-200" />
-            </div>
-            <div className=" mt-5 ">
-              <button
-                type="button"
-                onClick={getCurrentLocation}
-                className="inline-flex items-center gap-2 bg-gray-200 rounded-md px-3 py-2 text-black font-semibold whitespace-nowrap"
-              >
-                <MapPinned size={18} className=" text-orange-400" />
-                current location
-              </button>
-            </div>
-          </form>
-          <div className="rounded-2xl border border-orange-100 bg-linear-to-br from-orange-50 via-white mt-5 to-amber-50 p-5 shadow-2xl">
+          <label className="block mb-2 font-semibold">Drop location</label>
+          <div className="relative">
+            <Flag
+              className="absolute left-3 top-1/2 -translate-y-1/2 text-red-600"
+              size={18}
+            />
+            <Input
+              value={drop}
+              onChange={(e) => {
+                setDrop(e.target.value);
+                searchLocation(e.target.value);
+              }}
+              className="pl-10"
+              placeholder="Drop location"
+            />
+          </div>
+
+          <div className="flex gap-5">
+            <button
+              type="button"
+              className="mt-5 inline-flex items-center gap-2 bg-gray-200 rounded-md px-3 py-2 font-semibold"
+            >
+              Book Ride
+            </button>
+
+            <button
+              type="button"
+              className="mt-5 inline-flex items-center gap-2 bg-gray-200 rounded-md px-3 py-2 text-red-400 font-semibold"
+            >
+              Cancel Ride
+            </button>
+          </div>
+
+          <div className="flex items-center gap-4 my-6">
+            <div className="flex-1 h-px bg-gray-200" />
+            <div className="flex-1 h-px bg-gray-200" />
+          </div>
+
+          <div className="mt-5">
+            <button
+              type="button"
+              onClick={getCurrentLocation}
+              className="inline-flex items-center gap-2 bg-gray-200 rounded-md px-3 py-2 font-semibold"
+            >
+              <MapPinned size={18} className="text-orange-400" />
+              current location
+            </button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <LeafletMap />
+        </div>
+        {/*  <div className="rounded-2xl border border-orange-100 bg-linear-to-br from-orange-50 via-white mt-5 to-amber-50 p-5 shadow-2xl">
             <div className="mb-4 flex flex-wrap gap-2">
               <span className="rounded-full bg-orange-500 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-gray-600">
                 SwiftRide
@@ -177,9 +164,8 @@ const Page = () => {
               focuses on a secure, smooth, and convenient mobility experience
               for both riders and drivers.
             </p>
-          </div>
-        </div>
-      </LoadScript>
+          </div>* */}
+      </div>
     </>
   );
 };
--- client/src/components/ui/map.tsx
+++ client/src/components/ui/map.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Fix marker icon issue in Next.js
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 export default function Map() {
   return (
     <MapContainer
       center={[23.3441, 85.3096]}
       zoom={13}
-      style={{ height: "400px", width: "100%" }}
+      style={{ height: "400px", width: "100%", borderRadius: "16px" }}
     >
       <TileLayer
         attribution="© OpenStreetMap contributors"
