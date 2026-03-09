ef16470899a4a2a9770b6de9b3b6caa89f512486
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-03-09 11:29:23 +0530

    work on live tracking service

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppShell from "../components/layout/AppShell";
+import "leaflet/dist/leaflet.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
