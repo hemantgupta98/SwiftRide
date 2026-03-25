69a70eaaad52dc0b08ca97ecbba02793d2f64fa7
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-03-25 15:33:57 +0530

    update redirect

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,5 @@
-import HomePage from "./home/page";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+  return redirect("/home");
 }
